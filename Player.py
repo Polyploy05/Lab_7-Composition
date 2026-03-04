a1dfd331a194ffbd848bd3e2b6b33654bf2e09ff
DMOC1211 <145091883+DMOC1211@users.noreply.github.com>
2026-03-04 11:29:29 -0800

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,32 +1,46 @@
 
 
 
-import Die
+from die import Die
 class Player:
     def __init__(self):
         d1 = Die(6)
         d2 = Die(6)
         d3 = Die(6)
-        self.dice = [d1, d2, d3]
-        self.points = 0
+        self._dice = [d1, d2, d3]
+        self._dice.sort()
+        self._points = 0
 
+    @property
     def points(self):
-        return self.points
+        return self._points
     
     def roll_dice(self):
         for die in self.dice:
             die.roll()
+        self._dice.sort()
     
     def has_pair(self):
-        for i in range(len(self.dice)):
-            for j in range(i+1, len(self.dice)):
-                if self.dice[i] == self.dice[j]:
-                    self.points += 1
-                    return True
+        if self._dice[0] == self._dice[1] or self._dice[1] == self._dice[2]:
+            self._points += 1
+            return True
         return False
                 
     def has_three_of_a_kind(self):
-        if self.dice[0] == self.dice[1] and self.dice[1] == self.dice[2]:
+        if self._dice[0] == self._dice[1] and self._dice[1] == self._dice[2]:
             self.points += 3
             return True
         return False
+
+    def has_series(self):
+        v1 = self._dice[0].value
+        v2 = self._dice[1].value
+        v3 = self._dice[2].value
+
+        if v2 - v1 == 1 and v3 - v2 == 1:
+            self._points += 2
+            return True
+        return False
+
+    def __str__(self):
+        return f"D1 = {self._dice[0].value}, D2 = {self._dice[1].value}, D3 = {self._dice[2].value}"
