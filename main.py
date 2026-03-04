30824fe0c33cc9a96e52449063cf0206962bf9b1
Polyploy05 <jakada005@gmail.com>
2026-03-04 11:36:41 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Group:
 Description:
 '''
 
-from player import Player
+import Player
 from check_input import get_yes_no
 
 def take_turn(player):
@@ -15,36 +15,30 @@ def take_turn(player):
 
   print(player)
 
-  points = 0 
-
   # Checks if players gets three of a kind
   if player.has_three_of_a_kind():
     print("Three of a kind! +3 points")
-    points = 3
 
   # Only checks series if not three of a kind 
   elif player.has_series():
     print("Series! +2 points")
-    points = 2
 
-  # Only check pair iff they scored neither
+  # Only check pair if they scored neither
   elif player.has_pair():
     print("Pair! +1 point")
-    points = 1
     
   else:
     print("No combinations this turn.")
 
-  player.points += points
 
   print(f"Score this turn is completed.")
-  print(f"Current score: {player.points}")
+  print(f"Current score: " + str(player.points()))
 
 def main():
   
   print("Welcome to the Yahtzee!")
 
-  player = Player()
+  player = Player.Player()
 
   playing = True
   while playing:
@@ -52,7 +46,7 @@ def main():
     playing = get_yes_no("\nRoll again? (y/n): ")
 
   print("\nGame Over!")
-  print(f"Final Score: {player.points}")
+  print(f"Final Score: "+ str(player.points()))
 
 if __name__ == "__main__":
   main()
